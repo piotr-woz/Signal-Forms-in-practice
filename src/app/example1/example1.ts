b8a82ad6b5f550749c822b0f3169b60e966d0add
piotr-woz <piotrwozniak.dev@gmail.com>
2026-01-19 23:28:04 +0100

    Example1 – create initial form elements

--- src/app/example1/example1.ts
+++ src/app/example1/example1.ts
@@ -1,5 +1,8 @@
-import { Component, signal } from '@angular/core';
-import { form } from '@angular/forms/signals';
+import { Component, signal, ChangeDetectionStrategy } from '@angular/core';
+import { form, FormField, required, email } from '@angular/forms/signals';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
 
 type UserProfile = {
   firstName: string;
@@ -10,9 +13,10 @@ type UserProfile = {
 
 @Component({
   selector: 'app-example1',
-  imports: [],
+  imports: [MatFormFieldModule, MatInputModule, MatCardModule, FormField],
   templateUrl: './example1.html',
   styleUrl: './example1.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class Example1 {
   private readonly userProfile = signal<UserProfile>({
@@ -22,5 +26,9 @@ export default class Example1 {
     email: '',
   });
 
-  protected readonly userForm = form(this.userProfile);
+  protected readonly userForm = form(this.userProfile, (path) => {
+    (required(path.firstName, { message: 'This is a required field.' }),
+      required(path.lastName, { message: 'This is a required field.' }),
+      email(path.email, { message: 'The email address is not valid.' }));
+  });
 }
