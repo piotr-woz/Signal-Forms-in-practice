2b21ccb92089384aa70ce4caa325b24598fe6150
piotr-woz <piotrwozniak.dev@gmail.com>
2026-01-19 15:04:12 +0100

    Add signal-forms-1 component

---
+++ src/app/signal-forms-1/signal-forms-1.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-signal-forms-1',
+  imports: [],
+  templateUrl: './signal-forms-1.html',
+  styleUrl: './signal-forms-1.css',
+})
+export class SignalForms1 {}
