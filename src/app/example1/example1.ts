ccc5481e9496fd2aa0f56595587d418710a42731
piotr-woz <piotrwozniak.dev@gmail.com>
2026-01-19 21:26:04 +0100

    Add Angular Material to the project

--- src/app/example1/example1.ts
+++ src/app/example1/example1.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { form } from '@angular/forms/signals';
+
+type UserProfile = {
+  firstName: string;
+  lastName: string;
+  phone: string;
+  email: string;
+};
 
 @Component({
   selector: 'app-example1',
@@ -6,4 +14,13 @@ import { Component } from '@angular/core';
   templateUrl: './example1.html',
   styleUrl: './example1.css',
 })
-export default class Example1 {}
+export default class Example1 {
+  private readonly userProfile = signal<UserProfile>({
+    firstName: '',
+    lastName: '',
+    phone: '',
+    email: '',
+  });
+
+  protected readonly userForm = form(this.userProfile);
+}
