664c3d01e65f6acb24f29095d24a77aaf396f597
piotr-woz <piotrwozniak.dev@gmail.com>
2026-01-19 21:10:25 +0100

    Create home component HTML and styling

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -7,4 +7,42 @@ import { RouterLink } from '@angular/router';
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export default class Home {}
+export default class Home {
+  protected readonly examples = [
+    {
+      id: 1,
+      title: 'Basic Signal Form 1',
+      description: 'Podstawowa implementacja z signal inputs',
+    },
+    {
+      id: 2,
+      title: 'Basic Signal Form 2',
+      description: 'Podstawowa implementacja z signal inputs',
+    },
+    // {
+    //   id: 3,
+    //   title: 'Nested Form Groups',
+    //   description: 'Zagnieżdżone grupy formularzy',
+    // },
+    // {
+    //   id: 4,
+    //   title: 'Dynamic Form Arrays',
+    //   description: 'Dynamiczne tablice kontrolek',
+    // },
+    // {
+    //   id: 5,
+    //   title: 'Custom Validators',
+    //   description: 'Własne walidatory z signals',
+    // },
+    // {
+    //   id: 6,
+    //   title: 'Async Operations',
+    //   description: 'Asynchroniczne operacje i walidacja',
+    // },
+    // {
+    //   id: 7,
+    //   title: 'Form State Management',
+    //   description: 'Zarządzanie stanem formularza',
+    // },
+  ];
+}
