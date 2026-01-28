94fc096f7f81bc109552184761060bd4f71d3196
piotr-woz <piotrwozniak.dev@gmail.com>
2026-01-28 21:48:26 +0100

    Add ChangeDetectionStrategy.OnPush to home component

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -6,6 +6,7 @@ import { RouterLink } from '@angular/router';
   imports: [RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class Home {
   protected readonly examples = [
