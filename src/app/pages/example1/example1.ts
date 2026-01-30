3fa21546e31b96f29a501a9c6d194a08604fe163
piotr-woz <piotrwozniak.dev@gmail.com>
2026-01-30 01:01:36 +0100

    Example1 - Refactor component to include form submission and validation logic

--- src/app/pages/example1/example1.ts
+++ src/app/pages/example1/example1.ts
@@ -1,4 +1,4 @@
-import { Component, signal, ChangeDetectionStrategy } from '@angular/core';
+import { Component, signal, ChangeDetectionStrategy, computed } from '@angular/core';
 import {
   form,
   FormField,
@@ -13,18 +13,19 @@ import {
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
+import { MatButton } from '@angular/material/button';
 import { RouterLink } from '@angular/router';
 
-type UserProfile = {
+interface UserProfile {
   firstName: string;
   lastName: string;
   phone: string;
   email: string;
-};
+}
 
 @Component({
   selector: 'app-example1',
-  imports: [MatFormFieldModule, MatInputModule, MatCardModule, FormField, RouterLink],
+  imports: [MatFormFieldModule, MatInputModule, MatCardModule, FormField, RouterLink, MatButton],
   templateUrl: './example1.html',
   styleUrl: './example1.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -63,4 +64,12 @@ export default class Example1 {
     email(path.email, { message: 'The email address is not valid.' })),
       this.numericOnly(path.phone, { message: 'The phone number must contain only numbers.' }));
   });
+
+  protected readonly isFormValid = computed(
+    () =>
+      this.userForm.firstName().errors().length === 0 &&
+      this.userForm.lastName().errors().length === 0 &&
+      this.userForm.phone().errors().length === 0 &&
+      this.userForm.email().errors().length === 0,
+  );
 }
