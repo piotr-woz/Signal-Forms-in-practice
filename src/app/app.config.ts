b5805d57c37b189e731101b7d2e499687ef838a9
piotr-woz <piotrwozniak.dev@gmail.com>
2026-02-17 11:10:13 +0100

    Add provideCheckNoChangesConfig with exhaustive: true option

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,25 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  isDevMode,
+  provideCheckNoChangesConfig,
+} from '@angular/core';
 import { provideRouter, withViewTransitions } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes, withViewTransitions())],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes, withViewTransitions()),
+    ...(isDevMode()
+      ? [
+          // enable extra checks in dev mode only
+          provideCheckNoChangesConfig({
+            exhaustive: true,
+            interval: 3_000,
+          }),
+        ]
+      : []),
+  ],
 };
