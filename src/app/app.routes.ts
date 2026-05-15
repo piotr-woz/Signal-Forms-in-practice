86bab18c542f51e3d4dadb36266885d0982ff447
piotr-woz <piotrwozniak.dev@gmail.com>
2026-05-15 20:36:43 +0200

    chore: change application folder name from "pages" to "features"

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: 'home',
-    loadComponent: () => import('./pages/home/home'),
+    loadComponent: () => import('./features/home/home'),
   },
   {
     path: '',
@@ -12,10 +12,10 @@ export const routes: Routes = [
   },
   {
     path: 'example1',
-    loadComponent: () => import('./pages/example1/example1'),
+    loadComponent: () => import('./features/example1/example1'),
   },
   {
     path: 'example2',
-    loadComponent: () => import('./pages/example2/example2'),
+    loadComponent: () => import('./features/example2/example2'),
   },
 ];
